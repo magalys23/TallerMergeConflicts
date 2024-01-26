dbec700da3538ac12ac714e61923fec440d5735f
geno1995 <genoveva.chulde@gmail.com>
2024-01-26 09:56:53 -0500

    magaly 2 modificando

--- app/screens/ContactsScreen.js
+++ app/screens/ContactsScreen.js
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 
 export const Contacts = ({ navigation }) => {
     return <View style={styles.container}>
-        <Text>Estoy en contact</Text>
+        <Text>Estoy en contact Magaly Chulde</Text>
         <Button
             title='HOME'
             onPress={() => {
