396e8c1e113ef570724ba17cd82fa314fc0a9f0f
geno1995 <genoveva.chulde@gmail.com>
2024-01-26 09:51:21 -0500

    magaly 1

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#04B45F',
     alignItems: 'center',
     justifyContent: 'center',
   },
